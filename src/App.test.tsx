b794edf8dcdfc16befe497f4278c9dfc998a582b
PatrickHa <pattrickha@gmail.com>
2023-04-05 21:02:00 +0700

    feat: Update a test case rendering the homepage

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+test('renders MyReads title', () => {
+  render(<BrowserRouter>
+    <App />
+  </BrowserRouter>);
+
+  const linkElement = screen.getByText(/MyReads/i);
+  const linkElement2 = screen.getByText(/Currently Reading/i);
+  const linkElement3 = screen.getByText(/Want To Read/i);
+  const linkElement4 = screen.getByText(/^Read$/i);
+
   expect(linkElement).toBeInTheDocument();
+  expect(linkElement2).toBeInTheDocument();
+  expect(linkElement3).toBeInTheDocument();
+  expect(linkElement4).toBeInTheDocument();
 });
